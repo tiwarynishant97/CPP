b010544449968c174811f2c486de73818e620cbe
Nishant_Raj <69184209+tiwarynishant97@users.noreply.github.com>
2024-06-18 14:53:07 +0530

    static def added

--- concepts/static_const.cpp
+++ concepts/static_const.cpp
@@ -2,6 +2,9 @@
 
 //Firiend function can be global as well
 
+/*Static variables are for the class (not per object). i.e memory is allocated only once per class and every instance uses it. So if one object modifies its value then the modified value is visible to other objects as well. ( A simple thought.. To know the number of objects created for a class we can put a static variable and do ++ in constructor)
+Value persists between different function calls
+*/
 using namespace std ;
 const int a =10;
 static int b =10;
