513c93e2a375a2f8e58cced31fca7c56fb2e3a45
Nishant_Raj <69184209+tiwarynishant97@users.noreply.github.com>
2024-06-27 08:47:14 +0530

    Reference variable

---
+++ concepts/reference_variable.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+    
+    int a = 10;
+    int &y = a;
+    
+    cout <<" a val is "<<a <<" y val is "<<y<<endl;
+    cout <<" a add is "<<&a <<" y add is "<<&y<<endl;
+
+return 0;
+
+}
